9406503618f3e8fac54c773c799ab47785468e15
Erilin <AVErilin@WTC.loc>
2022-07-29 10:28:25 +0300

    init commint

---
+++ C++/xor.h
@@ -0,0 +1,20 @@
+#ifndef __XOR_H__
+#define __XOR_H__
+
+#include <iostream>
+
+class XOR{
+
+public:
+
+    static std::string update(std::string text, std::string key){
+        std::string data;
+        if(text.size() > 0 && key.size() > 0){
+            for(size_t index = 0; index < text.size(); index++)
+                data.push_back(text[index] ^ key[index % key.size()]);
+        }
+        return data;
+    }
+}; // CipherXOR
+
+#endif // __DPAPI_H__
